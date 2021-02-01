8e3d30f599b9e8e9fa9e3ba8d88e855c4e77c31e
BaherW <73704436+BaherW@users.noreply.github.com>
2021-02-01 03:39:35 +0100

    Added pawn movement and collision

--- app/App.svelte
+++ app/App.svelte
@@ -1,28 +1,95 @@
 <script>
+  import { Board , Pawn , spawnPieces , includes , emptyPiece } from './components/helper.ts'
 
-  import {Board} from './components/helper.ts'
+
+  var chessBoard = new Board()
+  chessBoard = spawnPieces(chessBoard);
+  var piecePicked = false;
+  var movedPiece;
+  var oldJ = 0;
+  var oldI = 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+  function clickHandler(i, j) {
+
+    if (piecePicked) {
+      releasePiece(i,j)
+
+    }
+    else {
+      pickPiece(i,j);
+    }
+
+    console.log(chessBoard.boardArray[i][j][1].name)
+
+  }
+
+
+  function pickPiece(i,j) {
   
-  var chessBoard = new Board();
+    piecePicked = true;
+    
+    movedPiece = chessBoard.boardArray[i][j][1];
+    chessBoard.boardArray[i][j][1].findValidMoves(chessBoard.boardArray);
 
-// board -> [a1 , a2 , a3] -> [a1, Piece.class] -> f , p
-  function clickHandler(i , j) {
-      console.log(chessBoard.boardArray[i][j])
-      
-      //isValidMove(chessBoard.boardArray[i][j].piece)
+    oldJ = j;
+    oldI = i;
+  }
+
+
+  function releasePiece(i, j) {
+    let pieceValidMoves = movedPiece.validMoves;
+    piecePicked = false;
+    modifyPiece()
+
+
+  function modifyPiece() {
+    if (includes(pieceValidMoves, [i,j])) {
+
+      chessBoard.boardArray[i][j][1] = movedPiece;
+      chessBoard.boardArray[i][j][1].position[0] = i;
+      chessBoard.boardArray[i][j][1].position[1] = j;
+      chessBoard.boardArray[i][j][1].validMoves = [];
+
+      chessBoard.boardArray[oldI][oldJ][1] = new emptyPiece(oldI , oldJ, "invis" , null, null , false) 
+      }
+    }
   }
 </script>
-
 
 <div>
   <div class="game-container">
     <div>
       {#each chessBoard.boardArray as row, i}
-        <div class=row>
+        <div class="row">
           {#each row as cell, j}
-            {#if ((j+1) % 2 === 1 && (i+1) % 2 === 0) || ((j+1) % 2 === 0 && (i+1) % 2 === 1)}
-              <div  on:click={()=> {clickHandler(i,j)}} class="cell white" id={chessBoard.boardArray[i][j]}></div>
+            {#if ((j + 1) % 2 === 1 && (i + 1) % 2 === 0) || ((j + 1) % 2 === 0 && (i + 1) % 2 === 1)}
+              <div on:click={() => {clickHandler(i, j)}} class="cell white"id={chessBoard.boardArray[i][j]}><img src={chessBoard.boardArray[i][j][1].icon}/></div>
             {:else}
-              <div on:click={()=> {clickHandler(i,j)}} class="cell black" id={chessBoard.boardArray[i][j]}></div>
+              <div
+                on:click={() => {clickHandler(i, j)}} class="cell black"id={chessBoard.boardArray[i][j]}><img src={chessBoard.boardArray[i][j][1].icon}/></div>
             {/if}
           {/each}
         </div>
@@ -31,33 +98,33 @@
   </div>
 </div>
 
-
-
-
 <style>
   .game-container {
-    transform: scaleY(-1);
-		position: relative;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
-  
+    transform:scaleY(-1);
+    color: white;
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
   .row {
-		display: flex;
-	}
-	.cell {
-		width: 65px;
-		height: 65px;
-	}
+    display: flex;
+  }
+  .cell {
+    width: 65px;
+    height: 65px;
+  }
 
   .black {
     background-color: rgb(63, 32, 15);
   }
 
   .white {
-    background-color: burlywood
+    background-color: burlywood;
   }
 
+  img {
+    transform:scaleY(-1);
+  }
 </style>
-
--- app/components/helper.ts
+++ app/components/helper.ts
@@ -1,6 +1,14 @@
 function num2chr(n: number):string {
     return String.fromCharCode(n+97);
-} 
+}
+
+export function includes(myArray: [number[]], checkArray: number[]):boolean {
+    for (let i = 0 ; i < myArray.length ; i++)
+        if (myArray[i][0] === checkArray[0] && myArray[i][1] === checkArray[1]) {
+            return true;
+        }
+    return false;
+}
 
 
 export class Board {
@@ -16,7 +24,7 @@ export class Board {
         for (let i = 0 ; i < 8 ; i++) {
             tempArray[i] = []
             for (let j = 0; j < 8; j++) {
-                tempArray[i][j] = [num2chr(j) + (i+1).toString() , null];
+                tempArray[i][j] = [num2chr(j) + (i+1).toString() , new emptyPiece(i,j,"invis" , "/chesspieces/invisible.png" , null , false)];
             }
         }
         return tempArray;
@@ -28,15 +36,75 @@ export abstract class Pieces {
 
     position = [];
     alive:boolean = true;
-    constructor(i , j) {
+    color:string;
+    icon:string;
+    name:string;
+    isPiece:boolean = true;
+    
+    constructor(i , j, color, icon , name , isPiece = true) {
         this.position = [i,j];
         this.alive = true;
+        this.color = color;
+        this.icon = icon;
+        this.name = name;
+        this.isPiece = isPiece
     }
 }
 
+export class emptyPiece extends Pieces {
+
+}
+
 export class Pawn extends Pieces {
+    validMoves:Array<[any,any]> = [];
+    isFirstMove = true;
+
+
+
+    private findValidMoves(board){
+        let [i , j] = this.position;
+        if (this.color === "black") {
+            if (this.isFirstMove) {
+                if (!board[i-1][j][1].isPiece) {
+                    this.validMoves.push([i-1, j])
+                }
+                if (!board[i-2][j][1].isPiece) {
+                    this.validMoves.push([i-2, j]);
+                }
+            
+                this.isFirstMove = false;
+            }
+            else {
+                if (!board[i-1][j][1].isPiece) {
+                    this.validMoves.push([i-1, j]);
+                }
+            }
+        }
+        else {
+            if (this.color === "white") {
+                if (this.isFirstMove) {
+                    if (!board[i-1][j][1].isPiece) {
+                        this.validMoves.push([i-1, j])
+                    }
+                    if (!board[i+2][j][1].isPiece) {
+                        this.validMoves.push([i+2, j]);
+                    }
+                
+                    this.isFirstMove = false;
+                }
+                else {
+                    if (!board[i+1][j][1].isPiece) {
+                        this.validMoves.push([i+1, j]);
+                    }
+                }
+            }
+        }
+    }
     
 
+
+    
+    
 }
 
 export class Knight extends Pieces {
@@ -45,7 +113,7 @@ export class Knight extends Pieces {
 }
 
 export class Bishop extends Pieces {
-    
+
 }
 
 export class Rook extends Pieces {
@@ -53,11 +121,29 @@ export class Rook extends Pieces {
 }
 
 export class Queen extends Pieces {
-    
 
 }
 
 export class King extends Pieces {
-    
 
+}
+
+
+
+
+
+
+
+export function spawnPieces(chessBoard:Board):Board {
+
+    for (let i = 0; i < chessBoard.boardArray[1].length; i++) {
+        chessBoard.boardArray[1][i][1] = new Pawn(1, i, "white", "/chesspieces/Chess_plt60.png" , "pawn");
+    }
+
+    for (let i = 0; i < chessBoard.boardArray[7].length; i++) {
+        chessBoard.boardArray[6][i][1] = new Pawn(6, i, "black", "/chesspieces/Chess_pdt60.png" , "pawn");
+    }
+
+    
+    return chessBoard;
 }
